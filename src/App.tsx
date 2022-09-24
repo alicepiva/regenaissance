e5535ec4bdbc7a85796374c7cda7adb84e72b63b
carollyb <carolly.barbosa@gmail.com>
2022-09-24 12:58:06 -0300

    :lipstick: adds react lineto library and tests line features

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import './App.css'
 import { teste } from "./content/teste"
+import LineTo from 'react-lineto';
 
 function App() {
 
   return (
-    <div className="App">
-      
-      <h1>tcc de alice</h1>
-      <div className="card">
-        <p>
-          {teste.content}
-        </p>
-        <p>
-          {teste.link}
-        </p>
+    <>
+      <h1>regenascença</h1>
+      <div className="App">
+        
+        <div>
+          <div className="card">
+            <p>
+              {teste.content}
+            </p>
+          </div>
+          <div className="card2">
+            <p>
+              {teste.responsabilidade}
+            </p>
+          </div>
+          <LineTo from="card" to="card2" fromAnchor='bottom' toAnchor='top' borderColor='white' />
+        </div>
+        <div className='design'>
+          <a  className='textoDesign' href={teste.link}  target="_blank">discrete design</a>
+        </div>
+        <LineTo from="card" to="design" fromAnchor='right' toAnchor='left' borderColor='white' />
+        
       </div>
-      
-    </div>
+    </>
   )
 }
 
--- src/content/teste.ts
+++ src/content/teste.ts
@@ -1,4 +1,5 @@
 export const teste = {
-    content: 'teste blasbdlasbdkl',
-    link: 'teste link'
+    content: 'necessidade de repensar a atuação do design como ferramenta empoderadora para processos de justiça socioambiental',
+    responsabilidade: 'a responsabilidade do design como um campo técnico responsável por ter materializado visões de mundo hegemônicas antropocêntricas',
+    link: 'https://www.e-flux.com/architecture/becoming-digital/248060/discrete-automation/'
 }
